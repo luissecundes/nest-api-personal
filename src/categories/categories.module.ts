2bd4ba1827488f2de18927785ded770aa7419265
Luis Henrique <luissecundes@gmail.com>
2024-10-10 17:24:23 -0300

    Aula de módulos

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CategoriesController } from './categories.controller';
-import { CategoriesRepository } from './categories.repository';
 
 @Module({
   imports: [],
-  controllers: [CategoriesController],
-  providers: [CategoriesRepository],
+  controllers: [],
+  providers: [],
 })
 export class CategoriesModule {}
