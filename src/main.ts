8b7c8d5d307f113142c747309018c5e271ef7220
Luis Henrique <luissecundes@gmail.com>
2024-10-10 17:42:55 -0300

    alteração para projeto original

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
