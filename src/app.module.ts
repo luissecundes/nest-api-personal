d9c96c170e74405b97f3bf89f7b7081ab7437297
Luis Henrique <luissecundes@gmail.com>
2024-10-10 17:33:58 -0300

    alteração do projeto para a base do nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
 @Module({
-  imports: [CategoriesModule, ProductsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/categories/dto/list-category.dto.ts
+++
@@ -1,8 +0,0 @@
-export class ListCategoryDTO {
-  constructor(
-    readonly id: string,
-    readonly name: string,
-    readonly desc: string,
-    readonly date: Date,
-  ) {}
-}
