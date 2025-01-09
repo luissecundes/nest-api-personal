cdec6170fd324fb1119587510c922249ac7a1835
Luis Henrique <luissecundes@gmail.com>
2025-01-09 08:15:13 -0300

    criado entity

--- src/app/recados/recado.service.ts
+++ src/app/recados/recado.service.ts
@@ -1,10 +1,8 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class RecadoService { 
-    hello() {
-        return 'Hello World!';
-    }
-    
+export class RecadoService {
+  hello() {
+    return 'Hello World!';
+  }
 }
